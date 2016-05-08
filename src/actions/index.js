a92588de0da547d697c2134917593ec83178e4c3
noriyotcp <noriyo.akita@gmail.com>
2016-05-08 11:25:51 +0900

    Import axios and pass the request to return value as payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = 'cd3f1af6c2105536d4e3ceff646509f9';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&{city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
